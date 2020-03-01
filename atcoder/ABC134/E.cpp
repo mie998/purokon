40e6ce18fcec5e0a335aff6a217a6a3acb6c351d
MIE999 <kei.west.post920@gmail.com>
2020-03-01 18:55:18 +0900

    ABC134

--- atcoder/ABC134/E.cpp
+++ atcoder/ABC134/E.cpp
@@ -1,33 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// def
 #define debug(x) cout << #x << ": " << x << endl
 #define out(x) cout << x << endl
+#define repeat(i, a, b) for (int i = (a); i < (b); i++)
+#define revrepeat(i, a, b) for (int i = (b)-1; i >= (a); i--)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define CYES cout << "Yes" << endl
+#define CNO cout << "No" << endl
+#define SPC(x) cout << fixed << setprecision(x)
+#define ZERO(a) memset(a, 0, sizeof(a))
+#define MINUS(a) memset(a, 0xff, sizeof(a))
+
 typedef long long ll;
-const int M = 1e9 + 7;
-const int INF = 1e9;
-const int MAX_V = 300 + 5;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vll;
+typedef pair<int, int> P;
+typedef complex<ld> Point;
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const int MAX_V = 1e5 + 5;
 const int MAX_N = 1e5 + 5;
+const double PI = acos(-1);
+// LIS(longest increasing subsequence)
+// http://even-eko.hatenablog.com/entry/2013/09/05/215236
+int dp[100005];
+int lis(vector<ll> &a) {
+    int n = a.size();
+    fill(dp, dp + n, INF);
+    for (int i = 0; i < n; ++i) {
+        *upper_bound(dp, dp + n, a[i]) = a[i];
+    }
+    return lower_bound(dp, dp + n, INF) - dp;
+}
 
 int main() {
     int n;
     cin >> n;
-    vector<int> a(n);
-    int rst = 1;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    priority_queue<int, vector<int>, less<int>> colors;
-    colors.push(a[0]);
-    for (int i = 1; i < n; i++) {
-        if (a[i] <= colors.top()) {
-            rst++;
-            colors.push(a[i]);
-        } else {
-            colors.pop();
-            colors.push(a[i]);
-        }
-    }
-    cout << rst << endl;
+    vl a(n);
+    rep(i, n) cin >> a[i];
+    reverse(all(a));
+    int ans = lis(a);
+    out(ans);
 }
