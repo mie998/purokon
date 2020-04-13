b277063c9cc7149555145d08513554fc680dc9a8
mie998 <kei.west.post920@gmail.com>
2020-04-14 00:16:18 +0900

    fix library

--- library/CPP/utils/utils.cpp
+++ library/CPP/utils/utils.cpp
@@ -155,6 +155,34 @@ void line(const Type &a) {
 }
 
 // import from https://rsk0315.github.io/library/library/utility/make/vector.cpp.html
+// for c++14
+namespace detail {
+template <typename Tp, size_t N>
+std::vector<Tp> make_vector(
+    std::vector<size_t> &sizes,
+    typename std::enable_if<(N == 1), Tp const &>::type x) {
+    return std::vector<Tp>(sizes[0], x);
+}
+template <typename Tp, size_t N>
+auto make_vector(
+    std::vector<size_t> &sizes,
+    typename std::enable_if<(N > 1), Tp const &>::type x) {
+    size_t size = sizes[N - 1];
+    sizes.pop_back();
+    return std::vector<decltype(make_vector<Tp, N - 1>(sizes, x))>(
+        size, make_vector<Tp, N - 1>(sizes, x));
+}
+} // namespace detail
+
+template <typename Tp, size_t N>
+auto make_vector(size_t const (&sizes)[N], Tp const &x = Tp()) {
+    std::vector<size_t> s(N);
+    for (size_t i = 0; i < N; ++i)
+        s[i] = sizes[N - i - 1];
+    return detail::make_vector<Tp, N>(s, x);
+}
+
+// for c++17
 namespace detail {
 template <typename Tp, size_t Nb>
 auto make_vector(std::vector<size_t> &sizes, Tp const &x) {
