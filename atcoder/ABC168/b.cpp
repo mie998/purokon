fa868ae87f1aed21049c53d6415833eaf07e15c2
MIE999 <kei.west.post920@gmail.com>
2020-05-17 23:13:42 +0900

    ABC168

---
+++ atcoder/ABC168/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//def
+#define debug(x) cout << #x << ": " << x << '\n'
+#define out(x) cout << x << '\n'
+#define repeat(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define revrepeat(i, a, b) for (int i = (int)(b)-1; i >= (int)(a); i--)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (int)(n)-1; i >= 0; i--)
+#define foreach(e, v) for (auto &e : v)
+#define all(x) (x).begin(), (x).end()
+#define CYES cout << "Yes" << endl
+#define CNO cout << "No" << endl
+#define SPC(x) cout << fixed << setprecision(x)
+#define ZERO(a) memset(a, 0, sizeof(a))
+#define MINUS(a) memset(a, 0xff, sizeof(a))
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vll;
+typedef pair<int, int> P;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = __INT_MAX__;               // 2^31 - 1
+constexpr long long INFLL = __LONG_LONG_MAX__; // 2^61 - 1
+constexpr int MAX_N = 1e5 + 5;
+constexpr double PI = acos(-1);
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int k;
+    string s;
+    cin >> k >> s;
+    if (s.size() <= k) {
+        out(s);
+        return 0;
+    }
+
+    s = s.substr(0, k);
+    s.push_back('.');
+    s.push_back('.');
+    s.push_back('.');
+    out(s);
+
+    return 0;
+}
--- library/CPP/graph/Dijkstra.cpp
+++ library/CPP/graph/Dijkstra.cpp
@@ -1,7 +1,9 @@
 int prev[MAX_V]; // 最短路の直前の頂点
+int d[MAX_V];
+bool used[MAX_V];
 
 //　始点sから各頂点への最短距離をもとめる
-void difkstra(int s) {
+void dijkstra(int s) {
     fill(d, d + V, INF);
     fill(used, used + V, false);
     fill(prev, prev + V, -1);
@@ -34,5 +36,47 @@ vector<int> get_path(int t) {
     reverse(path.begin(), path.end());
     return path;
 }
-
 // すべての辺のコストが等しい時、単一始点最短路は幅優先探索で求めることができる。この場合はダイクストラ法のpriority_queueはqueueと同じ振る舞いをする。
+
+// https://drken1215.hatenablog.com/entry/2019/02/15/141000
+// グラフ
+using Edge = pair<int, long long>;  // {辺の行き先, 重み}
+using Graph = vector<vector<Edge>>; // グラフ
+// 使われうる辺集合
+set<pair<int, int>> use;
+
+// s を始点とする Dijkstra
+vector<long long> Dijkstra(const Graph &G, int s) {
+    vector<long long> dist((int)G.size(), INF);
+    vector<vector<int>> prev((int)G.size()); // prev[v] := v から復元できる辺たち
+    dist[s] = 0;
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> que;
+    que.push({0, s});
+    while (!que.empty()) {
+        auto cur = que.top();
+        que.pop();
+        long long cur_dist = cur.first;
+        int v = cur.second;
+        if (cur_dist > dist[v]) continue; // 効く枝刈り
+        for (auto e : G[v]) {
+            if (dist[e.first] > dist[v] + e.second) {
+                dist[e.first] = dist[v] + e.second;
+                que.push({dist[e.first], e.first});
+
+                // 復元のための
+                prev[e.first].clear();
+                prev[e.first].push_back(v);
+            } else if (dist[e.first] == dist[v] + e.second) {
+                prev[e.first].push_back(v);
+            }
+        }
+    }
+
+    // 使われうる辺集合
+    for (int v = 0; v < (int)G.size(); ++v) {
+        for (auto u : prev[v]) {
+            use.insert(make_pair(min(u, v), max(u, v)));
+        }
+    }
+    return dist;
+}
